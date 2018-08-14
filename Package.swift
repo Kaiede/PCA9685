27d9b0622f15da1574d4bee2a504bfed1bbed2ff
Adam Thayer <user@biticus.net>
2018-08-14 09:04:24 -0700

    Use Kaiede fork of SwiftyGPIO
    Unfortunately, this is required for the moment because RPiLight needs the fixes, and Swift 3 is too dumb to realize that the two forks are the same project.
    
    Swift 4 has fixed this issue though.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 let package = Package(
     name: "PCA9685",
     dependencies: [
-        .Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 1),
+    	// We need to use the fork for now. Sorry
+    	// A workaround for projects like RPiLight that depend on the fork.
+    	// Swift 4 doesn't have this problem, but Swift 3.1.1 does. 
+        .Package(url: "https://github.com/Kaiede/SwiftyGPIO.git", majorVersion: 1),
     ],
     swiftLanguageVersions: [3, 4]
 )
