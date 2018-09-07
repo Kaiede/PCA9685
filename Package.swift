246bb39cc842be6f7f9701b20c9d7018ea6fb50e
Adam Thayer <user@biticus.net>
2018-09-06 21:26:30 -0700

    Switch to SingleBoard for GPIO
    Start using SingleBoard instead of SwiftyGPIO. It is similar, but is a bit easier to bake in new functionality that makes it easier to write code against it.

--- Package.swift
+++ Package.swift
@@ -2,13 +2,21 @@
 
 import PackageDescription
 
+//
+// Backport of Package(url:from:)
+//
+extension Package.Dependency {
+    public class func Package(url: String, from versionString: String) -> Package.Dependency {
+        guard let version = Version(versionString) else { fatalError() }
+        let nextMajor = Version(version.major + 1, 0, 0)
+        return Package(url: url, versions: version..<nextMajor)
+    }
+}
+
 let package = Package(
     name: "PCA9685",
     dependencies: [
-    	// We need to use the fork for now. Sorry
-    	// A workaround for projects like RPiLight that depend on the fork.
-    	// Swift 4 doesn't have this problem, but Swift 3.1.1 does. 
-        .Package(url: "https://github.com/Kaiede/SwiftyGPIO.git", majorVersion: 1),
+        .Package(url: "https://github.com/Kaiede/SingleBoard.git", from: "0.1.1"),
     ],
     swiftLanguageVersions: [3, 4]
 )
